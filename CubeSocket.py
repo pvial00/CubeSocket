8ec6f7570843a6ba73e36805af29111c3e2f1412
Karl <pvial@krypt.local>
2017-09-02 00:05:59 -0700

    CubeSocket v0.2 Nonce support added

--- CubeSocket.py
+++ CubeSocket.py
@@ -2,13 +2,17 @@ from pycube90 import Cube
 import socket
 import os
 
-# v0.1.3
+# v0.2.0
 
 class CubeSocket:
-    def __init__(self, key):
+    def __init__(self, key, nonce_support=1, dc=0):
         self.key = key
         self.session_key = ""
+        self.nonce_support = nonce_support
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.key_length = 16
+        self.nonce_length = 16
+        self.direct_connect = dc
 
     def wrap(self, sock):
         self.sock = sock
@@ -16,13 +20,20 @@ class CubeSocket:
     def connect(self, host, port):
         self.sock.connect((host, port))
 
-    def send(self, buf):
-        buf = Cube(self.key).encrypt(buf)
-        self.sock.send(buf)
+    def send(self, buf, nonce=""):
+        if self.nonce_support == 0:
+            buf = Cube(self.key).encrypt(buf)
+            self.sock.send(buf)
+        elif self.nonce_support == 1:
+            buf = Cube(self.key, nonce).encrypt(buf)
+            self.sock.send(buf)
 
-    def recv(self, buf_size):
+    def recv(self, buf_size, nonce=""):
         buf = self.sock.recv(buf_size)
-        buf = Cube(self.key).decrypt(buf)
+        if self.nonce_support == 0:
+            buf = Cube(self.key).decrypt(buf)
+        elif self.nonce_support == 1:
+            buf = Cube(self.key, nonce).decrypt(buf)
         return buf
 
     def close(self):
@@ -36,18 +47,33 @@ class CubeSocket:
         self.sock.bind((host, port))
 
     def cli_keyexchange(self, sock):
-	session_key = self.gen_key(16)
-	self.send(session_key)
-	self.key = session_key
+        if self.nonce_support == 0:
+	    session_key = self.gen_key(self.key_length)
+    	    self.send(session_key)
+	    self.key = session_key
+        elif self.nonce_support == 1:
+            nonce = self.gen_key(self.nonce_length)
+            self.sock.send(nonce)
+	    session_key = self.gen_key(self.key_length)
+            key = Cube(self.key, nonce).encrypt(session_key)
+    	    self.sock.send(key)
+	    self.key = session_key
 
     def srv_keyexchange(self, client_sock):
-	session_key = client_sock.recv(16)
-	self.session_key = Cube(self.key).decrypt(session_key)
-	self.key = self.session_key
+        if self.nonce_support == 0:
+	    session_key = client_sock.recv(self.key_length)
+	    self.session_key = Cube(self.key).decrypt(session_key)
+	    self.key = self.session_key
+        elif self.nonce_support == 1:
+            nonce = client_sock.recv(self.nonce_length)
+            key = client_sock.recv(self.key_length)
+            session_key = Cube(self.key, nonce).decrypt(key)
+            self.key = session_key
 
     def accept(self):
         client, addr = self.sock.accept()
-	self.srv_keyexchange(client)
+        if self.direct_connect == 0:
+	    self.srv_keyexchange(client)
         return client, addr
 
     def gen_key(self, length):
@@ -62,19 +88,33 @@ class CubeSocket:
 	self.cli_keyexchange(self.sock)
 
     def cubesend(self, buf):
-        key = self.gen_key(16)
-        self.send(key)
-        buf = Cube(key).encrypt(buf)
-        self.sock.send(buf)
+        if self.nonce_support == 1:
+            nonce = self.gen_key(self.nonce_length)
+            self.sock.send(nonce)
+            buf = Cube(self.key, nonce).encrypt(buf)
+            self.sock.send(buf)
+        elif self.nonce_support == 0:
+            session_key = self.gen_key(self.key_length)
+            key = Cube(self.key).encrypt(session_key)
+            self.sock.send(key)
+            buf = Cube(session_key).encrypt(buf)
+            self.sock.send(buf)
 
     def cuberecv(self, buf_size):
-        key = self.recv(16)
-        buf = self.sock.recv(buf_size)
-        buf = Cube(key).decrypt(buf)
+        if self.nonce_support == 1:
+            nonce = self.sock.recv(self.nonce_length)
+            buf = self.sock.recv(buf_size)
+            buf = Cube(self.key, nonce).decrypt(buf)
+        elif self.nonce_support == 0:
+            key = self.sock.recv(self.key_length)
+            session_key = Cube(self.key).decrypt(key)
+            buf = self.sock.recv(buf_size)
+            buf = Cube(session_key).decrypt(buf)
         return buf
 
 class CubeWrap:
-    def __init__(self, sock, key):
+    def __init__(self, sock, key, nonce_support=1):
         self.sock = CubeSocket(key)
         self.sock.wrap(sock)
         self.raw_sock = sock
+        self.sock.nonce_support = nonce_support
